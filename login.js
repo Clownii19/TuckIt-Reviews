7120f6cfc6760d3cc9e7735c9b47ef846f3ad3a2
KyleDSouza <6472265-KyleDSouza@users.noreply.replit.com>
2023-11-09 06:51:31 +0000

    created rating system and about page

---
+++ login.js
@@ -0,0 +1,40 @@
+// This file contains the scripts for handling logins
+
+// This is a different API used for user logins
+const login_api_url = "https://retoolapi.dev/sHYcXV/login_data";
+
+// Defining async function
+async function check_login() {
+  // Storing response
+  const response = await fetch(login_api_url);
+
+  // Storing data in form of JSON
+  var data = await response.json();
+
+  // Get the inputted email and password
+  const emailText = document.getElementById("EmailText");
+  const passwordText = document.getElementById("PasswordText");
+
+  let loginSuccessful = false; // Add a flag to track login status
+
+  data.forEach(function(account) {
+    if (emailText.value == account.email && passwordText.value == account.password) {
+      alert("Welcome " + account.first_name + ".");
+
+      // Set the session to logged in
+      console.log("Setting session to logged in");
+      sessionStorage.setItem('userStatus', 'loggedIn');
+
+      location.reload();
+
+      loginSuccessful = true; // Mark login as successful
+    }
+  });
+
+  // If login was not successful, display an error alert
+  if (!loginSuccessful) {
+    alert("Incorrect login information. Please try again.");
+  }
+}
+
+
---
+++ menu.js
@@ -0,0 +1,51 @@
+function submitRating() {
+  const ratingInput = document.getElementById("rating");
+  const rating = parseFloat(ratingInput.value); // Parse the input as a floating-point number
+
+  if (!isNaN(rating) && rating >= 0 && rating <= 5) {
+    const apiUrl = "https://retoolapi.dev/HtkgFH/data";
+
+    fetch(apiUrl, {
+      method: "POST",
+      body: JSON.stringify({ rating: rating }), // Send the rating as a number
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Rating submitted successfully:", data);
+      })
+      .catch((error) => {
+        console.error("Error submitting rating:", error);
+      });
+  } else {
+    alert("Please enter a valid rating between 0 and 5.");
+  }
+}
+function fetchAndAverageItemRatings() {
+  const apiUrl = "https://retoolapi.dev/HtkgFH/data"; // Replace with your API URL
+
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      if (Array.isArray(data) && data.length > 0) {
+        // Calculate the average of item ratings
+        const totalRatings = data.reduce((acc, item) => acc + item.rating, 0);
+        const averageRating =  totalRatings/data.length ;
+
+        // Display the average rating in an HTML element with an ID
+        const averageRatingElement = document.getElementById("averageRating");
+        if (averageRatingElement) {
+          averageRatingElement.textContent = `Average Rating: ${averageRating.toFixed(2)}`;
+        } else {
+          console.error("Average rating element not found.");
+        }
+      } else {
+        console.error("No data found in the API response.");
+      }
+    })
+    .catch((error) => {
+      console.error("Error fetching data from the API:", error);
+    });
+}
